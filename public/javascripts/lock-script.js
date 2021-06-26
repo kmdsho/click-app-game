b17178bf0d8c3d6beb6bc828433da3648cf34d5f
kmdsho <iqjqeqwvmb19@gmail.com>
2021-06-26 16:45:18 +0900

    update

--- public/javascripts/lock-script.js
+++ public/javascripts/lock-script.js
@@ -4,22 +4,20 @@ const timer = setInterval(start => {
 
     console.log(remainTime);
 
-    if(remainTime < 0){
-        remainTime = 0;
-        clearInterval(timer);
+    if(remainTime >= 0){
+        let remainDate = new Date(remainTime);
+        let days = parseInt(remainTime / (1000 * 60 * 60 * 24));    //残り日数
+        let hour = remainDate.getUTCHours();
+        let min = remainDate.getUTCMinutes();
+        let sec = remainDate.getUTCSeconds();
+
+        if(days > 0){
+            dates += days + "日 ";
+        }
+
+        dates += hour + ":" + ("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2);
+        document.getElementById("unlock").innerHTML = dates;
+    }else{
         window.location.href = pathname;    //解除
     }
-
-    let remainDate = new Date(remainTime);
-    let days = parseInt(remainTime / (1000 * 60 * 60 * 24));    //残り日数
-    let hour = remainDate.getUTCHours();
-    let min = remainDate.getUTCMinutes();
-    let sec = remainDate.getUTCSeconds();
-
-    if(days > 0){
-        dates += days + "日 ";
-    }
-
-    dates += hour + ":" + ("0" + min).slice(-2) + ":" + ("0" + sec).slice(-2);
-    document.getElementById("unlock").innerHTML = dates;
 }, 100, new Date().getTime());
